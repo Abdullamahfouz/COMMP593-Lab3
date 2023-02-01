0375c0fbe5b1632957e6b793168ada23697add84
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-02-01 15:03:34 -0500

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -2,11 +2,15 @@ from sys import argv, exit
 import os
 from datetime import date
 import pandas as pd
+import regex as re
 
 def main():
     sales_csv = get_sales_csv()
     orders_dir = create_orders_dir(sales_csv)
+    order_id =
+    order_df = 
     process_sales_data(sales_csv, orders_dir)
+    export_order_to_excl(order_id, order_df, orders_dir)
     return
 # Get path of sales data CSV file from the command line
 def get_sales_csv():
@@ -52,11 +56,30 @@ def process_sales_data(sales_csv):
 # Insert a new "TOTAL PRICE" column into the DataFrame
   sales_df.insert (7, 'comment' , sales_df['ITEM QUANTITY'] * sales_df['ITEM PRICE'])
 # Remove columns from the DataFrame that are not needed
-
+  sales_df.drop(columns = ['ADDRESS', 'CITY' , 'STATE' , 'POSTAL CODE', 'COUNTRY'], inplace=True)
 # Group sales data by order ID and save to Excel sheets
-
-    
-    
+  for order_id, order_df in sales_df.groupby('ORDER ID'):
+    # Remove the 'ORDER ID' column
+   order_df.drop(columns=['ORDER ID'], inplace=True)
+   # Sort 
+   order_df.sort_values(by='ITEM NUMBER', inplace=True)
+   # Add the ground total row
+   grand_total = order_df['TOTAL PRIC3E'].sum()
+   grand_total_df = pd.DataFrame({'ITEAM PRICE' :  ['GRAND TOTAL:'], 'TOTAL PRICE':[grand_total]})
+   order_df = pd.concat([order_df, grand_total_df])
+   
+def export_order_to_excl(order_id, order_df, orders_dir):
+  
+  customer_name = order_df['CUSTOMER NAME'].values[0]
+  customer_name = re.sub(r'\W', '', customer_name)
+  order_file = f'Order{order_id}_{customer_name}.xlsx'
+  order_path = os.path.join(orders_dir, order_file)
+  os.path.join(orders_dir, order_file)
+  
+  sheet_name = f'Order # {order_id}'
+  order_df.to_excel(order_path)
+  
+  
     
 if __name__ == '__main__':
      main()
